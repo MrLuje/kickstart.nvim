b66c3af2e4116e78c7086141b14a8898f305f3dc
MrLuje <ogmniluje@gmail.com>
2025-02-13 08:06:08 +0100

    cmp-cmdline + cmp-cmdline-history

--- init.lua
+++ init.lua
@@ -894,6 +894,50 @@ require('lazy').setup({
     end,
   },
 
+  {
+    'hrsh7th/cmp-cmdline',
+    event = 'InsertEnter',
+    config = function()
+      local cmp = require 'cmp'
+      local config = cmp.get_config()
+      table.insert(config.sources, {
+        name = 'path',
+      })
+      table.insert(config.sources, {
+        name = 'cmdline',
+        option = {
+          ignore_cmds = { 'Man', '!' },
+        },
+      })
+      config.mapping = cmp.mapping.preset.cmdline()
+      -- `:` cmdline setup.
+      cmp.setup.cmdline(':', config)
+      -- `/` cmdline setup.
+      cmp.setup.cmdline('/', {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = 'buffer' },
+        },
+      })
+      cmp.setup(config)
+    end,
+  },
+
+  {
+    'dmitmel/cmp-cmdline-history',
+    event = 'InsertEnter',
+    config = function()
+      local cmp = require 'cmp'
+      local config = cmp.get_config()
+      table.insert(config.sources, {
+        name = 'cmdline_history',
+      })
+      config.mapping = cmp.mapping.preset.cmdline()
+      cmp.setup.cmdline(':', config)
+      cmp.setup(config)
+    end,
+  },
+
   { -- You can easily change to a different colorscheme.
     -- Change the name of the colorscheme plugin below, and then
     -- change the command in the config to whatever the name of that colorscheme is.
