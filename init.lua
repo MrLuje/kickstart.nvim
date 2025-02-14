e9be3613d0f528f745d6b3a833f26df2b47d92b1
Vincent De Oliveira <vincent.de-oliveira@harvest.fr>
2025-02-14 18:33:55 +0100

    disable autosession (kubectl issue)

--- init.lua
+++ init.lua
@@ -343,9 +343,9 @@ require('lazy').setup({
       auto_save = true, -- Enables/disables auto saving session on exit
       auto_restore = true, -- Enables/disables auto restoring session on start
       auto_create = true, -- Enables/disables auto creating new session files. Can take a function that should return true/false if a new session file should be created or not
-      auto_restore_last_session = true, -- On startup, loads the last saved session if session for cwd does not exist
+      auto_restore_last_session = false, -- On startup, loads the last saved session if session for cwd does not exist
 
-      suppressed_dirs = { '~/' }, -- Suppress session restore/create in certain directories
+      suppressed_dirs = { '~/', '/tmp' }, -- Suppress session restore/create in certain directories
       allowed_dirs = nil, -- Allow session restore/create in certain directories
       use_git_branch = false, -- Include git branch name in session name
       lazy_support = true, -- Automatically detect if Lazy.nvim is being used and wait until Lazy is done to make sure session is restored correctly. Does nothing if Lazy isn't being used. Can be disabled if a problem is suspected or for debugging
